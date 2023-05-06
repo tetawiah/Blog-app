bfe697bbb349a56538219822531b1446acd43d94
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2023-05-06 16:43:40 +0000

    add registration

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,3 +35,7 @@ Route::get('authors/{author:username}', function (User $author) {
         'posts' => $author->posts,
     ]);
 });
+
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
